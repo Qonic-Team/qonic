9fa873fa2a8fe27ea116d0484e7097e9d974c628
cogrpar <owen.r.welsh@gmail.com>
2022-09-28 21:56:37 -0600

    added build automation

--- source_dir/setup.py
+++ source_dir/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'qonic',
     packages = ['qonic'],
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'The Qonic project is an open source, expandable framework for solving problems using hybrid quantum computing solutions.',
     author = 'cogrpar',
     author_email = 'owen.r.welsh@hotmail.com',
