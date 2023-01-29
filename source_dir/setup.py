621324dbeb5cc66e2643f34a83d2c6db7eefcfc6
Owen Welsh <34008762+cogrpar@users.noreply.github.com>
2023-01-28 17:45:54 -0700

    Update setup.py
    new version with updated doc formatting

--- source_dir/setup.py
+++ source_dir/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'qonic',
     packages = ['qonic'],
-    version = '0.0.6',
+    version = '0.0.7',
     description = 'The Qonic project is an open source, expandable framework for solving problems using hybrid quantum computing solutions.',
     author = 'cogrpar',
     author_email = 'owen.r.welsh@hotmail.com',
